c50890b5cca214e5c41fb4cedc85687fddb9278e
Pehuén <pehuen.rodriguez@nayra.coop>
2021-01-26 16:15:57 -0300

    #2 Prepare things for test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   extends: ["standard", "plugin:prettier/recommended"],
+  plugins: ["jest"],
   rules: {
     "one-var": ["error", "never"],
     "prettier/prettier": "error",
+  },
+  env: {
+    "jest/globals": true,
   },
 };
