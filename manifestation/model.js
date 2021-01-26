d26ddffc476c10b4fc021b472529f93dccb41d60
unknown <degnexo@gmail.com>
2021-01-26 11:35:33 -0300

    manifestation model

--- manifestation/model.js
+++ manifestation/model.js
@@ -7,6 +7,7 @@ const ManifestationSchema = mongoose.Schema(
     title: { type: String, trim: true, required: true },
     description: { type: String, trim: true, required: true },
     footer: { type: String, trim: true, required: true },
+    stylesOverride: { type: String, trim: true, required: true },
     styles: {
       colors: {
         background: { type: String, trim: true, required: true },
@@ -15,19 +16,26 @@ const ManifestationSchema = mongoose.Schema(
       },
       // que posiblemente tenga forma de uri
       font: { type: String, trim: true, required: true },
-
+      cards: { 
+        columns: { type: Number, required: true },
+        width: { type: Number, required: true },
+        height: { type: Number, required: true },
+        colors: {
+          hover: { type: String, trim: true, required: true },
+          border: { type: String, trim: true, required: true }, 
+        }
+      }
     },
     images: {
-      header: { type: String, trim: true, required: true },
       header: { type: String, trim: true, required: true },
     },
     sponsors: [
       {
         name: { type: String, trim: true, required: true },
-        logo: { type: String, trim: true, required: true },
-        uri: { type: String, trim: true, required: true },
+        logoUri: { type: String, trim: true, required: true },
+        pageUri: { type: String, trim: true, required: true },
       }
-    ]
+    ],
     hashtags: [
       {
         // nota: hashtags en IG incluyen # y en TW no
@@ -38,6 +46,12 @@ const ManifestationSchema = mongoose.Schema(
         name: { type: String, trim: true, required: true },
         // const source_enum = ["instagram", "twitter"]
         source: { type: String, trim: true, required: true },
+      }
+    ],
+    metadata: [
+      {
+        name: { type: String, trim: true, required: true },
+        value: { type: String, trim: true, required: true },
       }
     ],
     startDate: { type: Date, required: true },
