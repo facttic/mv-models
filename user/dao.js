17a3ebc132651cc5744dfb68493c72545f7db28f
unknown <degnexo@gmail.com>
2021-01-29 12:28:17 -0300

    #45 added user CRUD

--- user/dao.js
+++ user/dao.js
@@ -6,6 +6,55 @@ const jwt = require("jsonwebtoken");
 
 const { UserSchema } = require("./model");
 
+UserSchema.statics.createNew = async function createNew(post) {
+  const _post = new UserDAO(post);
+  const newPost = await _post.save();
+  return newPost;
+};
+
+UserSchema.statics.udpate = async function udpate(_id, user) {
+  return await UserDAO.findByIdAndUpdate({ _id }, user, {
+    new: true,
+    runValidators: true,
+  }).exec();
+};
+
+UserSchema.statics.getAll = async function getAll({ skip, limit, sort, query }) {
+  const users = await UserSchema.find({ ...query })
+    .skip(skip)
+    .limit(limit)
+    .sort(sort);
+  return users;
+};
+
+UserSchema.statics.getById = async function getById(_id) {
+  return await UserDAO.findOne({ _id }).exec();
+};
+
+UserSchema.statics.removeById = async function removeById(_id, userId = null) {
+  return await UserDAO.delete({ _id }, userId);
+};
+
+UserSchema.statics.findByCredentials = async (email, password) => {
+  // Search for a user by email and password.
+  const user = await UserDAO.findOne({ email });
+  if (!user) {
+    throw new Error({ error: "Invalid login credentials" });
+  }
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+  if (!isPasswordMatch) {
+    throw new Error({ error: "Invalid login credentials" });
+  }
+  // let cleanUser = user.toObject();
+  // delete cleanUser.password;
+  return user;
+};
+
+
+UserSchema.statics.getAll = async function getAll({ query }) {
+  return await UserDAO.find({ ...query });
+};
+
 UserSchema.pre("save", async function (next) {
   // Hash the password before saving the user model
   const user = this;
@@ -22,21 +71,6 @@ UserSchema.methods.generateAuthToken = async function () {
   user.tokens = user.tokens.concat({ token });
   await user.save();
   return token;
-};
-
-UserSchema.statics.findByCredentials = async (email, password) => {
-  // Search for a user by email and password.
-  const user = await UserDAO.findOne({ email });
-  if (!user) {
-    throw new Error({ error: "Invalid login credentials" });
-  }
-  const isPasswordMatch = await bcrypt.compare(password, user.password);
-  if (!isPasswordMatch) {
-    throw new Error({ error: "Invalid login credentials" });
-  }
-  // let cleanUser = user.toObject();
-  // delete cleanUser.password;
-  return user;
 };
 
 UserSchema.plugin(mongooseDelete, {
