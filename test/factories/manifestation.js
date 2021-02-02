fc383110002442729cd78568096077d301c13642
Pehuén <pehuen.rodriguez@nayra.coop>
2021-02-02 16:53:49 -0300

    #48 manifestation model refactor

--- test/factories/manifestation.js
+++ test/factories/manifestation.js
@@ -5,29 +5,9 @@ module.exports = (factory, chance) => {
     name: chance.name(),
     uri: chance.url(),
     title: chance.sentence(),
+    subtitle: chance.sentence(),
     description: chance.paragraph(),
     footer: chance.paragraph(),
-    stylesOverride: chance.url(),
-    styles: {
-      colors: {
-        background: chance.color({ format: "hex" }),
-        foreground: chance.color({ format: "hex" }),
-        accent: chance.color({ format: "hex" }),
-      },
-      font: chance.url(),
-      cards: {
-        columns: chance.integer({ min: 1, max: 20 }),
-        width: chance.integer({ min: 1, max: 20 }),
-        height: chance.integer({ min: 1, max: 20 }),
-        colors: {
-          hover: chance.color({ format: "hex" }),
-          border: chance.color({ format: "hex" }),
-        },
-      },
-    },
-    images: {
-      header: chance.url(),
-    },
     sponsors: [
       {
         name: chance.company(),
@@ -41,22 +21,62 @@ module.exports = (factory, chance) => {
         source: chance.pickone(["twitter", "instagram"]),
       },
     ],
-    metadata: [
-      {
-        name: chance.word(),
-        value: chance.sentence(),
+    metadata: {
+      title: chance.sentence(),
+      keywords: chance.sentence(),
+      description: chance.sentence(),
+    },
+
+    crawlStatus: chance.n(() => buildCrawlStatus(chance), chance.d20()),
+    styles: {
+      colors: {
+        background: chance.color({ format: "hex" }),
+        accent: chance.color({ format: "hex" }),
       },
-    ],
+      text: {
+        title: {
+          font: chance.url(),
+          color: chance.color({ format: "hex" }),
+        },
+        subtitle: {
+          font: chance.url(),
+          color: chance.color({ format: "hex" }),
+        },
+        body: {
+          font: chance.url(),
+          color: chance.color({ format: "hex" }),
+        },
+      },
+      thumbnails: {
+        columns: chance.pickone([7, 8, 9, 10]),
+        colors: {
+          hover: chance.color({ format: "hex" }),
+          border: chance.color({ format: "hex" }),
+        },
+      },
+      cards: {
+        darkMode: chance.bool(),
+      },
+    },
+    images: {
+      header: chance.url(),
+      favicon: chance.url(),
+      og: {
+        twitter: chance.url(),
+        facebook: chance.url(),
+      },
+    },
     startDate: chance.date(),
     endDate: chance.date(),
     people: chance.integer(),
-    crawlStatus: [
-      {
-        post_id_str: chance.word(),
-        post_created_at: chance.word(),
-        hashtag: chance.word(),
-        source: chance.pickone(["twitter", "instagram"]),
-      },
-    ],
   });
 };
+
+function buildCrawlStatus(chance) {
+  return {
+    post_id_str: chance.word(),
+    post_created_at: chance.word(),
+    hashtag: chance.word(),
+    source: chance.pickone(["twitter", "instagram"]),
+  };
+}
