6bec0a0eed115a143d56a580ae122a5c681725b9
unknown <degnexo@gmail.com>
2021-02-05 15:52:13 -0300

    #7 defualts added to manifestation model
    Adapted toJSON to change _id to id for admin requirements fields

--- manifestation/model.js
+++ manifestation/model.js
@@ -5,68 +5,77 @@ const ManifestationSchema = mongoose.Schema(
   {
     name: { type: String, trim: true, required: true },
     uri: { type: String, trim: true, required: true },
-    title: { type: String, trim: true, required: true },
-    subtitle: { type: String, trim: true, required: true },
-    description: { type: String, trim: true, required: true },
-    footer: { type: String, trim: true, required: true },
-    sponsors: [
-      {
-        name: { type: String, trim: true, required: true },
-        logoUri: { type: String, trim: true, required: true },
-        pageUri: { type: String, trim: true, required: true },
-      },
-    ],
-    hashtags: [
-      {
-        // nota: hashtags en IG incluyen # y en TW no
-        // TODO: normalizar todo sin almohadita
-        type: HashtagSchema,
-        required: true,
-      },
-    ],
-    metadata: {
-      title: { type: String, trim: true, required: true },
-      keywords: { type: String, trim: true, required: true },
-      description: { type: String, trim: true, required: true },
+    title: { type: String, trim: true, default: "" },
+    subtitle: { type: String, trim: true, default: "" },
+    description: { type: String, trim: true, default: "" },
+    footer: { type: String, trim: true, default: "" },
+    sponsors: {
+      type: [
+        {
+          name: { type: String, trim: true, required: true },
+          logoUri: { type: String, trim: true, required: true },
+          pageUri: { type: String, trim: true, required: true },
+        },
+      ],
+      default: undefined,
     },
-    crawlStatuses: [
-      {
-        post_id_str: { type: String, trim: true, required: true, index: true },
-        post_created_at: { type: String, trim: true, required: true },
-        hashtag: { type: String, trim: true },
-        source: { type: String, trim: true, required: true, enum: ["twitter", "instagram"] },
-      },
-    ],
+    hashtags: {
+      type: [
+        {
+          // nota: hashtags en IG incluyen # y en TW no
+          // TODO: normalizar todo sin almohadita
+          type: HashtagSchema,
+          required: true,
+        },
+      ],
+      default: undefined,
+    },
+    metadata: {
+      title: { type: String, trim: true, default: "" },
+      keywords: { type: String, trim: true, default: "" },
+      description: { type: String, trim: true, default: "" },
+    },
+    crawlStatuses: {
+      type: [
+        {
+          post_id_str: { type: String, trim: true, required: true, index: true },
+          post_created_at: { type: String, trim: true, required: true },
+          hashtag: { type: String, trim: true },
+          source: { type: String, trim: true, required: true, enum: ["twitter", "instagram"] },
+        },
+      ],
+      default: undefined,
+    },
     // podemos evaluarlo, pero por ahora no lo vamos a usar.
     // stylesOverride: { type: String, trim: true, required: true },
     styles: {
       colors: {
-        background: { type: String, trim: true, required: true },
+        background: { type: String, trim: true, default: "" },
         // se usará para texto: links, hashtags
-        accent: { type: String, trim: true, required: true },
+        accent: { type: String, trim: true, default: "" },
       },
       text: {
         title: {
-          font: { type: String, trim: true, required: true },
-          color: { type: String, trim: true, required: true },
+          font: { type: String, trim: true, default: "" },
+          color: { type: String, trim: true, default: "" },
         },
         subtitle: {
-          font: { type: String, trim: true, required: true },
-          color: { type: String, trim: true, required: true },
+          font: { type: String, trim: true, default: "" },
+          color: { type: String, trim: true, default: "" },
         },
         // se usará para text de cards posts body
         body: {
-          font: { type: String, trim: true, required: true },
-          color: { type: String, trim: true, required: true },
+          font: { type: String, trim: true, default: "" },
+          color: { type: String, trim: true, default: "" },
         },
       },
       thumbnails: {
         // TODO: habría un map entre cantidad de columnas y tamaños
         // 7: columnas 500px x 500px
-        columns: { type: Number, required: true, enum: [7, 8, 9, 10] },
+        columns: { type: Number, enum: [7, 8, 9, 10] },
         colors: {
-          hover: { type: String, trim: true, required: true },
-          border: { type: String, trim: true, required: true },
+          hover: { type: String, trim: true, default: "" },
+          border: { type: String, trim: true, default: "" },
         },
       },
       cards: {
@@ -74,16 +83,16 @@ const ManifestationSchema = mongoose.Schema(
       },
     },
     images: {
-      header: { type: String, trim: true, required: true },
-      favicon: { type: String, trim: true, required: true },
+      header: { type: String, trim: true, default: "" },
+      favicon: { type: String, trim: true, default: "" },
       og: {
-        twitter: { type: String, trim: true, required: true },
-        facebook: { type: String, trim: true, required: true },
+        twitter: { type: String, trim: true, default: "" },
+        facebook: { type: String, trim: true, default: "" },
       },
     },
-    startDate: { type: Date, required: true },
-    endDate: { type: Date, required: true },
-    people: { type: Number, required: true },
+    startDate: { type: Date, default: Date.now },
+    endDate: { type: Date, default: Date.now },
+    people: { type: Number, default: 0 },
   },
   { collection: "manifestation" },
 );
